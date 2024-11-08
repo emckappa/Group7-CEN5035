3f1a43f24b7cc1b06b2e4a6803d599a29cba55e2
AmosFord <aford.af@gmail.com>
2024-11-07 21:51:26 -0500

    fixed header link

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,7 @@ const header = () => {
     <div className="container">
     <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start ">
         <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
-        <img src="/public/owlhead-logo.png" alt="fau owl owlhead-logo" className="header-logo"></img>
+        <img src="/owlhead-logo.png" alt="fau owl owlhead-logo" className="header-logo"></img>
         <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
         </a>
 
