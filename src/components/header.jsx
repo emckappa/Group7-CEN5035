abfece0bab77e162e8ca867366c6af481691e2d4
carl <crlvillarosa@gmail.com>
2024-11-18 09:57:37 -0500

    db env

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,10 +14,10 @@ const header = () => {
         </a>
 
         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="/home" className="nav-link px-2 link-secondary">Home</a></li>
+        {/* <li><a href="/home" className="nav-link px-2 link-secondary">Home</a></li>
         <li><a href="/applications" className="nav-link px-2 link-body-emphasis">Applications</a></li>
         <li><a href="/courses" className="nav-link px-2 link-body-emphasis">Courses</a></li>
-        <li><a href="/jobposting" className="nav-link px-2 link-body-emphasis">Job Posting</a></li>
+        <li><a href="/jobposting" className="nav-link px-2 link-body-emphasis">Job Posting</a></li> */}
         </ul>
 
         <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
